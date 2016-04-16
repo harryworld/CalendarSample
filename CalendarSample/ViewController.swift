2de1206e6f2f72d384b2746f0f1b53d023bcc09c
Harry Ng <harryworld@gmail.com>
2016-04-16 21:26:45 +0800

    Update project with library update

--- CalendarSample/ViewController.swift
+++ CalendarSample/ViewController.swift
@@ -13,13 +13,18 @@ class ViewController: NSViewController {
     
     let calendarView = HNCalendarView()
 
+    @IBOutlet weak var containerView: NSView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         addChildViewController(calendarView)
-        calendarView.view.frame = view.frame
+        calendarView.view.frame = containerView.frame
         view.addSubview(calendarView.view)
+        
+        calendarView.counts = generateCounts()
+        calendarView.selectedDate = NSDate()
     }
 
     override var representedObject: AnyObject? {
@@ -28,6 +33,15 @@ class ViewController: NSViewController {
         }
     }
 
+    func generateCounts() -> [Int] {
+        
+        var counts = [Int]()
+        for i in 0..<NSDate().monthDays {
+            counts.append(i % 2)
+        }
+        
+        return counts
+    }
 
 }
 
